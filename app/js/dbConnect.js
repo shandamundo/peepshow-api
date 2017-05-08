9d1c10972e04f6420e4d5d1c112b67cfc9714e02
James <james_shand@live.co.uk>
2017-05-08 23:44:47 +0100

    removal of public and restructure of files

--- app/js/dbConnect.js
+++ app/js/dbConnect.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import dbConfig from './../db/dbconfig.json';
+import dbConfig from './db/dbconfig.json';
 
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host:dbConfig.host,
